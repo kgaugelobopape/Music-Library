9bbc3c30e1d8499e01f3fde5ca63bdf44e62743c
Kgaugelo Bopape <kgaugelo20@gmail.com>
2018-09-26 21:22:35 +0200

    remove cover when deleting album

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -8,32 +8,16 @@ use Validator, File;
 
 class AlbumController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return view('index', ['albums' => Album::all()]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $form_data = $request->all();
@@ -58,7 +42,6 @@ class AlbumController extends Controller
         }
 
         $album = new Album;
-
         $album->name = $request->get('name');
         $album->cover = $name;
         $album->genre = $request->get('genre');
@@ -69,35 +52,16 @@ class AlbumController extends Controller
         return redirect()->route('list')->with('success', 'New album has been added!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Album $album
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         return view('show', ['album' => Album::find($id)]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Album $album
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         return view('edit', ['album' => Album::find($id)]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  \App\Album $album
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         $form_data = $request->all();
@@ -131,22 +95,18 @@ class AlbumController extends Controller
         $album->genre = $request->get('genre');
         $album->year = $request->get('year');
         $album->artist = $request->get('artist');
-
         $album->save();
+
         return redirect()->route('list')->with('success', 'Album has been updated');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Album $album
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         $album = Album::find($id);
         $album->reviews()->delete();
         $album->delete();
+        unlink(public_path().'/covers/'.$album->cover);
+
         return redirect()->route('list')->with('success', 'Album has been deleted');
     }
 }
